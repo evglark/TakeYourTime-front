7ab10dc9b6b461ac64a9a7a3d5167c19ba168727
Jewgenij Larkin <jewglark@Jewgenijs-MacBook-Pro.local>
2023-11-30 17:59:16 +0100

    Added Promotions and PriceByPhoto

--- src/components/MainPage/PriceByPhoto/index.tsx
+++ src/components/MainPage/PriceByPhoto/index.tsx
@@ -1,8 +1,47 @@
 import React from 'react';
+import Image from 'next/image';
+
+import messenger from './icons/messenger.svg';
+import telegram from './icons/telegram.svg';
+import whatsapp from './icons/whatsapp.svg';
 import './style.scss';
 
 export const PriceByPhoto = () => {
   return (
-    <div>5</div>
+    <div className="price-by-photo-component _flex _justify-center">
+      <div className="white-bg">
+        <div className="_flex _flex-col">
+          <div className="title _flex _justify-center">
+            {'Get the price from the photo \n with 99% accuracy'}
+          </div>
+          <div className="text _flex _justify-center">
+            {'Send a photo of the room and comments in any convenient \n messenger, and we will make an accurate calculation for you'}
+          </div>
+          <div className="img-wrapper _flex _justify-center">
+            img
+          </div>
+        </div>
+        <div className="_flex _justify-center _gap-6">
+          <div className="social-button _flex _justify-center _gap-2 _cursor-pointer">
+            <div>
+              <Image width={24} height={24} src={telegram} alt='' />
+            </div>
+            <div>Telegram</div>
+          </div>
+          <div className="social-button _flex _justify-center _gap-2 _cursor-pointer">
+            <div>
+              <Image width={24} height={24} src={whatsapp} alt='' />
+            </div>
+            <div>Whatsapp</div>
+          </div>
+          <div className="social-button _flex _justify-center _gap-2 _cursor-pointer">
+            <div>
+              <Image width={24} height={24} src={messenger} alt='' />
+            </div>
+            <div>Messenger</div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 };
--- src/components/MainPage/Promotions/index.tsx
+++ src/components/MainPage/Promotions/index.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import Image from 'next/image';
+
+import gift from './images/gift.svg';
+import parentAndChildren from './images/parent-and-children.svg';
+import student from './images/student.svg';
 import './style.scss';
 
 export const Promotions = () => {
+  const promotions = [
+    { img: gift, text: 'Make the first order Promo code TYT - 20% off your first order' },
+    { img: student, text: 'Students, save big on your cleaning services with us! Use promo code STUDENTCLEAN for 10% off' },
+    { img: parentAndChildren, text: 'Large family? Enjoy more family time, less cleaning time. Get 10% off with promo code FAMILYCLEAN' },
+  ];
+
   return (
-    <div>6</div>
+    <div className="promotions-component _flex _flex-col">
+      <div className="title">Promotions and discounts</div>
+      <div className="promotions-block _flex _justify-around">
+        {promotions.map((el) => (
+          <div className="promotion-card">
+            <div className="_flex _justify-center">
+              <Image width={128} height={128} src={el.img} alt='' />
+            </div>
+            <div className="text">{el.text}</div>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 };
