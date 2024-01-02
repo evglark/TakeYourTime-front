1fbb69bb8a559b5ae6c39695af6745cbc1a0ac43
Jewgenij Larkin <jewglark@Jewgenijs-MacBook-Pro.local>
2024-01-02 22:22:50 +0100

    Added people to main screen

--- src/components/MainPage/MainOffer/index.tsx
+++ src/components/MainPage/MainOffer/index.tsx
@@ -2,10 +2,9 @@
 import React from 'react';
 import { useRouter } from 'next/navigation';
 
-// import png from './mainOffer.png';
-import { Man } from './icons/Man';
-import { Woman } from './icons/Woman';
-import { MainOfferSVG } from './icons/MainOffer';
+import mainPng from './images/main.png';
+import manPng from './images/man.png';
+import womanPng from './images/women.png';
 import { CheckSvg } from './icons/check-fill';
 import './style.scss';
 
@@ -18,11 +17,19 @@ export const MainOffer = (props: any) => {
     { title: t('Fair'), text: t('price') },
   ];
 
+  const getImgStyle = (img: string) => ({
+    backgroundImage: `url(${img})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+  });
+
+  console.log('mainPng', mainPng);
+
   return (
-    <div className="main-offer-wrapper">
-      <div className="main-svg">
-        <MainOfferSVG />
-      </div>
+    <div className="main-offer-wrapper" style={getImgStyle(mainPng.src)}>
+      <div className="people-wrapper-man" style={getImgStyle(manPng.src)} />
+      <div className="people-wrapper-woman" style={getImgStyle(womanPng.src)} />
       <div className="vector-wrapper">
         <svg xmlns="http://www.w3.org/2000/svg" width="597" height="523" viewBox="0 0 597 523" fill="none">
           <path fillRule="evenodd" clipRule="evenodd" d="M287.048 1.87918C372.352 0.788133 467.246 -11.8611 529.087 46.9061C591.729 106.433 608.657 204.766 589.131 288.945C572.407 361.05 496.589 394.68 437.945 439.842C390.829 476.125 346.258 516.056 287.048 521.579C221.883 527.657 155.523 512.62 105.105 470.888C49.7983 425.11 15.1905 360.149 5.99187 288.945C-4.67255 206.396 -7.10886 112.495 51.1219 53.0193C109.644 -6.75415 203.403 2.94901 287.048 1.87918Z" fill="#EBEFFF" fillOpacity="0.93"/>
