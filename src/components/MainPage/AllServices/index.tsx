d59ee9bffa91947e2f8d255a4cc04273127cd35e
Jewgenij Larkin <jewglark@Jewgenijs-MacBook-Pro.local>
2024-01-09 20:47:13 +0100

    Added modal service

--- src/components/MainPage/AllServices/index.tsx
+++ src/components/MainPage/AllServices/index.tsx
@@ -1,30 +1,34 @@
 import React, { useState } from 'react';
-import Image from 'next/image';
 
 import { Overlay } from '@/components/common/Overlay';
 import { Slider } from '@/components/common/Slider';
 import { ServiceItem } from '@/components/common/ServiceItem';
+import { useClickOutside } from '@/hooks/useClickOutSide';
 
 import Airbnb from './icons/airbnb.svg';
 import Brush from './icons/brush.svg';
-import BurningTime from './icons/burning-time.svg'
+import BurningTime from './icons/burning-time.svg';
 import Confetti from './icons/confetti.svg';
 import Clean from './icons/clean.svg';
-import DiscountEmail from './icons/discount-email.svg'
+import DiscountEmail from './icons/discount-email.svg';
 import Ecology from './icons/ecology.svg'
 import Kitchen from './icons/kitchen.svg';
 import List from './icons/list.svg';
 import Mask from './icons/mask.svg';
 import Office from './icons/office.svg';
 import OzoneLayer from './icons/ozone-layer.svg';
-import Sofa from './icons/sofa.svg'
-import WeMoveToNewHouse from './icons/we-move-to-new-house.svg'
-import Window from './icons/window.svg'
+import Sofa from './icons/sofa.svg';
+import WeMoveToNewHouse from './icons/we-move-to-new-house.svg';
+import Window from './icons/window.svg';
+
+import { Modals } from './Modals';
 import './style.scss';
 
 export const AllServices = (props: any) => {
   const { t } = props;
-  const [active, setActive] = useState(false);
+  const [active, setActive] = useState('');
+  const ref = useClickOutside(() => setActive(''));
+
   const services = [
     { title: 'Regular', icon: Clean },
     { title: 'Dry cleaning', icon: Sofa },
@@ -59,9 +63,9 @@ export const AllServices = (props: any) => {
 
   return (
     <div className="all-service-component">
-      <Overlay active={active}>
-        <div>
-          123
+      <Overlay active={!!active}>
+        <div ref={ref}>
+          <Modals title={active} onClose={() => setActive('')} t={t} />
         </div>
       </Overlay>
       <div className="main-title">{t('All service')}</div>
@@ -70,8 +74,8 @@ export const AllServices = (props: any) => {
           id: el.id,
           content: (): JSX.Element => (
             <div className="_px-2.5 _flex _flex-col _w-full _gap-6">
-              <ServiceItem title={t(el.coll[0].title)} icon={el.coll[0].icon} t={t} onClick={() => setActive(true)} />
-              <ServiceItem title={t(el.coll[1].title)} icon={el.coll[1].icon} t={t} onClick={() => setActive(true)} />
+              <ServiceItem title={t(el.coll[0].title)} icon={el.coll[0].icon} t={t} onClick={() => setActive(el.coll[0].title)} />
+              <ServiceItem title={t(el.coll[1].title)} icon={el.coll[1].icon} t={t} onClick={() => setActive(el.coll[1].title)} />
             </div>
           )
         })))}
